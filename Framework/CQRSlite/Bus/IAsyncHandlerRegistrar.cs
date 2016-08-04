7cd6bfdf5db5b28daa5e1954fc7961b56ea56562
Pieter Jansen van Vuuren <pieter.jvv@gmail.com>
2016-08-04 12:54:45 +1200

    Oops missed a file

---
+++ Framework/CQRSlite/Bus/IAsyncHandlerRegistrar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using CQRSlite.Messages;
+
+namespace CQRSlite.Bus
+{
+    public interface IAsyncHandlerRegistrar
+    {
+        void RegisterHandler<T>(Func<T, Task> handler) where T : IMessage;
+    }
+}
